8f6ba36c1027f355e7e81dd70c26ee48b1af3d6c
K-V-G <k.goworin@gmail.com>
2023-07-05 16:22:29 +0700

    Проведены изменения в соответствие с отчетом Сергея

--- src/test/java/ru/service/NewsServiceImplTest.java
+++ src/test/java/ru/service/NewsServiceImplTest.java
@@ -1,18 +1,29 @@
 package ru.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.repository.NewsRepoImpl;
+import ru.repository.NewsTypeRepo;
 
-import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ExtendWith(value = SpringExtension.class)
 class NewsServiceImplTest {
+    @Autowired
+    NewsRepoImpl newsRepo;
+
+    @Autowired
+    NewsTypeRepo newsTypeRepo;
+
 
     @Test
     void findAll() {
+
     }
 
     @Test
